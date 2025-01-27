82be7f7f5934b9e0f6c79805b49ad48f33eb985f
Darlan Rodrigues Schmeller <darlanschimeller@gmail.com>
2025-01-27 15:24:55 -0300

    complete tic tac toe game for milestone project

--- Notebooks/Milestone_Project/displaying_information.py
+++ Notebooks/Milestone_Project/displaying_information.py
@@ -9,26 +9,36 @@ def display(row1, row2, row3):
     
 
 def user_choice():
-    user_input_value = None
+    # VARIABLES
+    
+    # Initial
+    choice = 'WRONG'
+    acceptable_range = range(0,10)
+    within_range = False
 
-    while not type(user_input_value) == int:
-        try:
-            user_input_value = input('Select a number for the position: ')
-            user_input_value = int(user_input_value)
-            if user_input_value not in range(1,4):
-                print("Please choose between 1 and 3")
-                user_input_value = None
-        except:
-            print('Not a number, try again.')
+    while choice.isdigit() == False or within_range == False:
+        
+        choice = input('Select a number for the position: ')
 
+        # Digit Check
+        if choice.isdigit() == False:
+            print("Sorry that is not a digit.")
 
+        # Range Check
+        if choice.isdigit():
+            if int(choice) in acceptable_range:
+                within_range = True
+            else:
+                print("Number entered not in the correct range")
+                within_range = False
+    return int(choice)
 
-    row1[user_input_value-1] = 'X'
 
 row2[1] = 'X'
 
 def game():
-    user_choice()
+    user_chosen_position = user_choice()
+    row1[user_chosen_position] = 'X'
     display(row1, row2, row3)
 
 game()
